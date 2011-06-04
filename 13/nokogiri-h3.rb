22ad800b0774b3143ca0fae056d043a40386d614
Tomohiko Himura <eiel.hal@gmail.com>
2011-06-04 14:45:24 +0900

    nokogiri-h3.rb

---
+++ 13/nokogiri-h3.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+
+doc = Nokogiri::HTML(open("http://www.ruby-lang.org/ja/"),nil,"utf-8")
+doc.css("h3").each do |h3|
+  puts h3.text
+end
+puts doc.css("h3").size
